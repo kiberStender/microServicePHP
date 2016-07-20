a058b26bb3de7b3346bfe2f549d5b124d617a4bb
Kleber Stender <kleber.stender@gmail.com>
2016-07-20 19:43:23 -0300

    Created index for dbReader project

---
+++ dbReader/index.php
@@ -0,0 +1,23 @@
+<?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  
+  include './autoloader.php';
+  
+  use br\com\microServicePjDemo\dbreader\controller\Controller;
+  use fp\result\ResFailure;
+  
+  /**
+   * 
+   * @return \fp\result\Result
+   */
+  function main(){
+    return Controller::controller()->select(json_decode($_POST['data']));
+  }
+  
+  try {
+    echo json_encode(main());
+  } catch (Exception $e){
+    echo "{\"failed\":true,\"decription\":\"$e\"}";
+  }
