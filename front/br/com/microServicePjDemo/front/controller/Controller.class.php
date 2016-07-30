495c4dc8b240e1411db78ae139b7d5d874c7c2ad
Kleber Stender <kleber.stender@gmail.com>
2016-07-30 15:45:06 -0300

    Now router url is dinamically setted in conf.properties file

--- front/br/com/microServicePjDemo/front/controller/Controller.class.php
+++ front/br/com/microServicePjDemo/front/controller/Controller.class.php
@@ -1,9 +1,10 @@
 <?php
 
   namespace br\com\microServicePjDemo\front\controller;
-  
+
   use fp\result\ResFailure;
   use fp\result\ResSuccess;
+  use fp\config\Configuration;
 
   /**
    * Description of Controller
@@ -25,43 +26,45 @@
       }
       return self::$controller_;
     }
-    
-    private function curlJson($url, $data){
+
+    private function curlJson($url, $data) {
       $ch = curl_init($url);
-              
+
       curl_setopt($ch, CURLOPT_POST, true);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/x-www-form-urlencoded'));
       curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('data' => $data)));
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-              
+
       //execute post
       $res = curl_exec($ch);
       $error = curl_error($ch);
       curl_close($ch);
-              
-      if($error){
+
+      if ($error) {
         return ResFailure::failure("Error accessing: $url. $error");
       } else {
         $result = json_decode($res);
-        
-        if($result->failed){
+
+        if ($result->failed) {
           return ResFailure::failure($result->description);
         } else {
           return ResSuccess::success($result->result);
         }
       }
     }
-    
-    public function login(string $user, string $password){
-      $data = array(
-          'endpoint' => 'auth',
-          'data' => json_encode(array('username'=> $user, 'password'=> $password))
-      );
-      
-      $url = 'http://localhost/microServicePHP/router/?type=request';
-      
-      return $this->curlJson($url, json_encode($data));
+
+    public function login(string $user, string $password) {
+      return Configuration::config()->getString('router.url')->map(function($routerUrl) {
+            $data = array(
+                'endpoint' => 'auth',
+                'data' => json_encode(array('username' => $user, 'password' => $password))
+            );
+            return $this->curlJson($routerUrl, json_encode($data));
+          })->getOrElse(function() {
+            
+          });
     }
+
   }
   
